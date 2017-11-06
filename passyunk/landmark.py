7163d9beca2a8021f45c73f2c7075c4d9bdaa92f
CITY\Alex.waldman <alex.waldman@phila.gov>
2017-11-06 16:35:52 -0500

    Handle null landmark names.

--- passyunk/landmark.py
+++ passyunk/landmark.py
@@ -2,13 +2,14 @@ import os, sys
 import csv
 import re
 import string
-from fuzzywuzzy import process, fuzz
+from fuzzywuzzy import process
 from .namestd import StandardName
 
 
 class Landmark:
     def __init__(self, item):
         self.item = item
+        self.landmark_name = ''
         self.landmark_address = ''
         self.is_landmark = False
 
@@ -18,7 +19,7 @@ class Landmark:
         return os.path.join(cwd, file_name + '.csv')
 
     def list_landmarks(self, first_letter):
-        path = self.csv_path('landmarks')
+        path = self.csv_path('standardized_landmarks')
         landmark_dict = {}
         try:
             with open(path, 'r') as f:
@@ -26,9 +27,8 @@ class Landmark:
                 for row in reader:
                     # Don't match on 'the' as first word
                     rlist = row[0].split()
-                    rlist = rlist[1:] if rlist[0].lower() == 'the' else rlist
-                    # lname = row[0].lower()
-                    lname = ' '.join(rlist).lower()
+                    rlist = rlist[1:] if rlist[0] == 'THE' else rlist
+                    lname = ' '.join(rlist)
                     if lname[0] != first_letter:
                         continue
                     landmark_dict[lname] = row[1]
@@ -42,18 +42,26 @@ class Landmark:
         tmp_list = re.sub('[' + string.punctuation + ']', '', tmp).split()
         std = StandardName(tmp_list, False).output
         # Don't match on 'the' if first word
-        tmp = ' '.join(std[1:]) if std[0].lower() in ('the', 'teh') else ' '.join(std)
+        try:
+            tmp = ' '.join(std[1:]) if std[0].upper() in ('THE', 'TEH') else ' '.join(std)
+        except:
+            tmp = tmp.upper()
         # Fuzzy matching:
-        first_letter = tmp[0].lower()
+        try:
+            first_letter = tmp[0]
+        except:
+            first_letter = ''
         landmark_dict = self.list_landmarks(first_letter)
-        landmark_list = [x.lower()[1:] for x in landmark_dict.keys()]
-        results = process.extract(tmp.lower()[1:], landmark_list, limit=3)
+        landmark_list = [x for x in landmark_dict.keys()]
+        results = process.extract(tmp, landmark_list, limit=3)
         results = sorted(results, key=lambda r: r[1], reverse=True)
         try:
-            lname = tmp[0].lower() + results[0][0]
-            landmark_address = landmark_dict[lname] if results[0][1] > 85 else ''
+            lname = results[0][0]
+            landmark_address = landmark_dict[lname] if results[0][1] > 89 else ''
             self.is_landmark = True if landmark_address else False
             self.landmark_address = landmark_address
+            self.landmark_name = lname
         except:
+            print("error: ", results)
             pass
 
