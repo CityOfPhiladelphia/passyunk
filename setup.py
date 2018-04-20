f167612302e5c50109a403ba35b06800ae16507b
Alexander M. Waldman <awaldman1@gmail.com>
2018-04-20 15:48:36 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='passyunk',
     version='1.0',
-    packages=[''],
+    packages=['passyunk'],
     url='',
     license='',
     author='tom.swanson',
