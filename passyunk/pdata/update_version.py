3ee58b92303854d38537c53eeb89fd25ac2cc196
floptical <stephen.macdavid@gmail.com>
2022-10-17 16:02:02 -0400

    New deploy key and updates to update_version

--- passyunk/pdata/update_version.py
+++ passyunk/pdata/update_version.py
@@ -5,22 +5,24 @@ def update_version():
     '''
     Increment the minor version of setup.py - not intended to be run by Passyunk users
     '''
-    # Read in the file
-    with open('setup.py', 'r') as file :
-        filedata = file.read()
+    try: 
+        # Read in the file
+        with open('../../setup.py', 'r') as file:
+            filedata = file.read()
+        
+        # Replace the target string
+        version_text = re.findall(f'(?<=version=\').*?(?=\')', filedata)[0]
+        old_version = Version(version_text)
+        new_version = old_version.increment_minor()
+        filedata = filedata.replace(f"version='{old_version.version}'", f"version='{new_version.version}'") 
+        
+        # Write the file out again
+        with open('../../setup.py', 'w') as file:
+            file.write(filedata)
 
-    # Replace the target string
-    version_text = re.findall(f'(?<=version=\').*?(?=\')', filedata)[0]
-    old_version = Version(version_text)
-    new_version = old_version.increment_minor()
-    print(f'Updating from version {old_version.version} to {new_version}')
-    filedata = filedata.replace(f"version='{old_version.version}'", f"version='{new_version}'") 
-    
-    # Write the file out again
-    with open('setup.py', 'w') as file:
-        file.write(filedata)
-    
-    return new_version
+        print(f'{new_version.version}')
+    except Exception as e: 
+        raise e
 
 if __name__ == '__main__': 
     update_version()
