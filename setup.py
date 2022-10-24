d121e86af06d5e53514c8fd39d298f61c6c3e849
auto-citygeo <maps@phila.gov>
2022-10-24 11:36:24 -0400

    Testing Post Develop and Install Commands

--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
 from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        develop.run(self)
+        print('PostDevelopCommand')
+        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        print('PostInstallCommand')
+        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
 
 setup(
     name='passyunk',
@@ -12,5 +28,9 @@ setup(
     author='tom.swanson',
     author_email='',
     description='address parser for City of Philadelphia addresses',
-    install_requires=['fuzzywuzzy']
+    install_requires=['fuzzywuzzy'], 
+    cmdclass={
+        # 'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+    },
 )
