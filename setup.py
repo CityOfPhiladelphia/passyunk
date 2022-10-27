eaa944e94135fb0a588bb1da43afc18c55d1ac34
James Midkiff <james.midkiff@phila.gov>
2022-10-27 12:02:19 -0400

    Deleted requirements.txt and moved into setup.py

--- setup.py
+++ setup.py
@@ -12,5 +12,10 @@ setup(
     author='tom.swanson',
     author_email='',
     description='address parser for City of Philadelphia addresses',
-    install_requires=['fuzzywuzzy', 'Levenshtein', 'requests'], 
+    install_requires=[
+        'fuzzywuzzy>=0.11.1,<1.0',
+        'Levenshtein>=0.20,<1.0',
+        'requests>=2.28,<3.0'
+    ], 
+    python_requires=">=3.6"
 )
