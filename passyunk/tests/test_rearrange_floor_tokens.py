4e40e74651ccd57b1bc3b8d163c644f64092d261
mbjackson-capp <mbjackson10@gmail.com>
2025-03-04 21:31:25 +0000

    atomize tests of rearrange_floor_tokens

--- passyunk/tests/test_rearrange_floor_tokens.py
+++ passyunk/tests/test_rearrange_floor_tokens.py
@@ -1,23 +1,39 @@
 from passyunk.rearrange_floor_tokens import rearrange_floor_tokens
 import pytest
 
-examples = {
-('MARKET', 'ST', '15F') : ['MARKET', 'ST', 'FL', '15'],
-('MARKET', 'ST', '15FL') : ['MARKET', 'ST', 'FL', '15'],
-('MARKET', 'ST', 'GROUND', 'FLOOR') : ['MARKET', 'ST', 'FLOOR', 'GROUND'],
-('MARKET', 'ST', 'FLOOR', '15', 'OFFICE') : ['MARKET', 'ST', 'OFFICE', 'FLOOR', '15'],
-('MARKET', 'ST', 'FLOOR', '15', 'APT') : ['MARKET', 'ST', 'APT', 'FLOOR', '15'],
-('MARKET', 'ST', 'UNIT', '#', '6', 'FLOOR', '#', '15') : ['MARKET', 'ST', 'UNIT', '#', '6', 'FLOOR', '#', '15'],
-('MARKET', 'ST', 'FLOOR', '#', '15', 'UNIT', '#', '6') : ['MARKET', 'ST', 'UNIT', '#', '6', 'FLOOR', '#', '15'],
-('MARKET', 'ST', 'GROUND', 'FLOOR', 'LBBY') : ['MARKET', 'ST', 'LBBY', 'FLOOR', 'GROUND'], # failing
-('MARKET', 'ST', 'FL', '#', '15', 'UNIT', '6') : ['MARKET', 'ST', 'UNIT', '6', 'FL', '#', '15'],
-('1234', 'MARKET', 'ST', 'FLOOR', '7', 'JUNK', 'TRASH', 'GARBAGE') : ['1234', 'MARKET', 'ST', 'JUNK', 'TRASH', 'GARBAGE', 'FLOOR', '7'],
-('1234', 'MARKET', 'ST', '15F', 'TRASH', 'NONSENSE'): ['1234', 'MARKET', 'ST', 'TRASH', 'NONSENSE', 'FL', '15'],
-}
 
-def test_rearrange_floor_tokens():
-    for k, v in examples.items():
-        assert rearrange_floor_tokens(list(k)) == v
+def test_basic1():
+    assert rearrange_floor_tokens(['MARKET', 'ST', '15F']) == ['MARKET', 'ST', 'FL', '15']
+
+def test_basic2():
+    assert rearrange_floor_tokens(['MARKET', 'ST', '15FL']) == ['MARKET', 'ST', 'FL', '15']
+
+def test_basic3():
+    assert rearrange_floor_tokens(['MARKET', 'ST', 'GROUND', 'FLOOR']) == ['MARKET', 'ST', 'FLOOR', 'GROUND']
+
+def test_basic4():
+    assert rearrange_floor_tokens(['MARKET', 'ST', 'FLOOR', '15', 'OFFICE']) == ['MARKET', 'ST', 'OFFICE', 'FLOOR', '15']
+
+def test_basic5():
+    assert rearrange_floor_tokens(['MARKET', 'ST', 'FLOOR', '15', 'APT']) == ['MARKET', 'ST', 'APT', 'FLOOR', '15']
+
+def test_asterisks1():
+    assert rearrange_floor_tokens(['MARKET', 'ST', 'UNIT', '#', '6', 'FLOOR', '#', '15']) == ['MARKET', 'ST', 'UNIT', '#', '6', 'FLOOR', '15']
+
+def test_asterisks2():
+    assert rearrange_floor_tokens(['MARKET', 'ST', 'FLOOR', '#', '15', 'UNIT', '#', '6']) == ['MARKET', 'ST', 'UNIT', '#', '6', 'FLOOR', '15']
+
+def test_asterisks3():
+    assert rearrange_floor_tokens(['MARKET', 'ST', 'FL', '#', '15', 'UNIT', '6']) == ['MARKET', 'ST', 'UNIT', '6', 'FL', '15']
+
+def test_lbby():
+    assert rearrange_floor_tokens(['MARKET', 'ST', 'GROUND', 'FLOOR', 'LBBY']) == ['MARKET', 'ST', 'LBBY', 'FLOOR', 'GROUND']
+
+def test_rearrange_trash():
+    assert rearrange_floor_tokens(['1234', 'MARKET', 'ST', 'FLOOR', '7', 'JUNK', 'TRASH', 'GARBAGE']) == ['1234', 'MARKET', 'ST', 'JUNK', 'TRASH', 'GARBAGE', 'FLOOR', '7']
+
+def test_rearrange_trash2():
+    assert rearrange_floor_tokens(['1234', 'MARKET', 'ST', '15F', 'TRASH', 'NONSENSE']) == ['1234', 'MARKET', 'ST', 'TRASH', 'NONSENSE', 'FL', '15']
 
 # Things that should not be changed by this function
 
@@ -30,8 +46,11 @@ def test_rearrange_unit_nf():
 def test_rearrange_pound_nf():
     assert rearrange_floor_tokens(['1234', 'MARKET', 'ST', '#', '3F']) == ['1234', 'MARKET', 'ST', '#', '3F']
 
+def test_rearrange_side_nf():
+    assert rearrange_floor_tokens('1009 S 8TH ST SIDE 2F'.split()) == '1009 S 8TH ST SIDE 2F'.split()
+
 def test_rearrange_streetnumber_ends_in_f():
     assert rearrange_floor_tokens('3411F SPRING GARDEN ST'.split()) == '3411F SPRING GARDEN ST'.split()
 
 def test_lstrip_0_from_floornum():
-    assert rearrange_floor_tokens('3405 ARTHUR ST # 01ST FL').split()) == ['3405', 'ARTHUR', 'ST', '#', 'FL', '1']
+    assert rearrange_floor_tokens('3405 ARTHUR ST # 01ST FL'.split()) == ['3405', 'ARTHUR', 'ST', '#', 'FL', '1']
