ab8b99440ec6b15b6efcf3546a31f8e457292e58
mbjackson-capp <mbjackson10@gmail.com>
2025-03-07 16:05:49 +0000

    add test to make sure get_zip_info() doesn't destroy information

--- passyunk/tests/test_zip4.py
+++ passyunk/tests/test_zip4.py
@@ -19,10 +19,21 @@ def private_installed():
 def test_zip4_1(p, private_installed):
     test_addr = "1 COMCAST CTR FL 32"
     ac = p.parse(test_addr)['components']
-    assert (private_installed and ac['mailing']['zip4'] == '2855') #incorrect base: 2833
+    assert private_installed 
+    assert ac['mailing']['zip4'] == '2855' #incorrect base: 2833
 
 def test_zip4_2(p, private_installed):
     test_addr = "1 S BROAD ST FL 11"
     ac = p.parse(test_addr)['components']
     print(ac['mailing']['zip4'])
-    assert (private_installed and ac['mailing']['zip4'] is None) #incorrect base: 3426
+    assert private_installed
+    assert ac['mailing']['zip4'] is None #incorrect base: 3426
+
+def test_zip4_unittype(p, private_installed):
+    """Ensure that get_zip_info() returns the proper unit type when floor is NOT involved"""
+    test_addr = "1130 SPRUCE ST # 1C"
+    ac = p.parse(test_addr)['components']
+    assert private_installed
+    assert ac['mailing']['zip4'] == '6004'
+    assert ac['address_unit']['unit_num'] == '1C'
+    assert ac['address_unit']['unit_type'] == 'APT'
